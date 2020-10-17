079f74d45d1ff4199a93286c6436bdacf3a69551
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-18 01:01:04 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan1/biodata/IF110119028Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk menampilkan biodata mahasiswa
+ */
+public class IF110119028Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Rizal Sihombing");
+        System.out.println("ALAMAT\t\t: Jalan Pagarsih Barat no 16");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 15 November 2020");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Jogging");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Cappuccino");
+    }
+    
+}
